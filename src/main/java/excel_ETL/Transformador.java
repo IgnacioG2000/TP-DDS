2f787fd2a44f3192480d2a2c852a5e197ec0eaaa
jfloresmaquera <jfloresm96@gmail.com>
2022-06-08 22:42:39 -0300

    Integracion de ETL de excel a java

---
+++ src/main/java/excel_ETL/Transformador.java
@@ -0,0 +1,61 @@
+package excel_ETL;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+
+import static org.apache.poi.ss.usermodel.CellType.STRING;
+
+public class Transformador {
+  public static void main(String[] args)
+  {
+    try
+    {
+      FileInputStream file = new FileInputStream(new File("howtodoinjava_demo.xlsx"));
+
+      //Create Workbook instance holding reference to .xlsx file
+      HSSFWorkbook workbook = new HSSFWorkbook(file);
+
+      //Get first/desired sheet from the workbook
+      HSSFSheet sheet = workbook.getSheetAt(0);
+
+      //Iterate through each rows one by one
+      Iterator<Row> rowIterator = sheet.iterator();
+      while (rowIterator.hasNext())
+      {
+        Row row = rowIterator.next();
+        //For each row, iterate through all the columns
+        Iterator<Cell> cellIterator = row.cellIterator();
+
+        while (cellIterator.hasNext())
+        {
+          Cell cell = cellIterator.next();
+          //Check the cell type and format accordingly
+          switch (cell.getCellType())
+          {
+            case NUMERIC:
+              System.out.print(cell.getNumericCellValue() + "t");
+              break;
+            case STRING:
+              System.out.print(cell.getStringCellValue() + "t");
+              break;
+          }
+        }
+        System.out.println("");
+      }
+      file.close();
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+}
+
+
