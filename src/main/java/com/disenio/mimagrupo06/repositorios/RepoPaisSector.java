49cbe06f805224e9502121085cc6cd0563f4b170
Rodri-Putrino <rputrino@frba.utn.edu.ar>
2022-11-05 20:40:20 -0300

    Agregamos query de reporte Composición HC Total A Nivel Pais discriminando provincias
    Co-authored-by: juliadp <28408244+juliadp@users.noreply.github.com>

--- src/main/java/com/disenio/mimagrupo06/repositorios/RepoPaisSector.java
+++ src/main/java/com/disenio/mimagrupo06/repositorios/RepoPaisSector.java
@@ -13,4 +13,10 @@ public interface RepoPaisSector extends CrudRepository<PaisSector, Long> {
   @Query(value="SELECT p.nombre, p.id FROM PaisSector p WHERE p.id = ?1")
   List<PaisSector> findAllById(Long id);
 
+  @Query(value = " SELECT ps.nombre AS PAIS, p.nombre AS PROVINCIA, COALESCE(SUM(hc.huellaCarbono), 0) AS HC_TOTAL" +
+                 " FROM ProvinciaSector p JOIN PaisSector ps ON p.pais.id = ps.id" +
+                 "                        LEFT JOIN ValorHCMensualSector hc ON p.id = hc.sector.id" +
+                 " GROUP BY ps.nombre, p.nombre")
+  List<PaisSector> findAllComposicionHCTotalANivelPais();
+
 }
