346162f43d73278e656c3de912c58218a0fd360f
Rodri-Putrino <rputrino@frba.utn.edu.ar>
2022-11-04 21:33:38 -0300

    Modificamos query reporte por sector territorial municipio
    Co-authored-by: juliadp <28408244+juliadp@users.noreply.github.com>

--- src/main/java/com/disenio/mimagrupo06/repositorios/RepoMunicipioSector.java
+++ src/main/java/com/disenio/mimagrupo06/repositorios/RepoMunicipioSector.java
@@ -1,12 +1,21 @@
 package com.disenio.mimagrupo06.repositorios;
 
-import com.disenio.mimagrupo06.apiDistancia.Municipio;
 import com.disenio.mimagrupo06.domain.sector.MunicipioSector;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface RepoMunicipioSector extends CrudRepository<MunicipioSector, Long> {
+
+    @Query(value = "SELECT m.nombre AS MUNICIPIO, SUM(hc.huellaCarbono) AS HC_TOTAL" +
+        "  FROM ProvinciaSector p JOIN PaisSector ps ON p.pais.id = ps.id" +
+        "                         JOIN MunicipioSector m ON p.id = m.provincia.id" +
+        "                         LEFT JOIN ValorHCMensualSector hc ON m.id = hc.sector.id" +
+        "  WHERE p.nombre = :nombre" +
+        "  GROUP BY m.nombre")
+    MunicipioSector findByNombre(String nombre);
 
     MunicipioSector findByMunicipioCodigo(Long municipioCodigo);
 }
