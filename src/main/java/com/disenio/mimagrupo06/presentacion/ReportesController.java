1bbbe0ee087277b2c04c73009fe090ed27ef0146
Rodri-Putrino <rputrino@frba.utn.edu.ar>
2022-11-13 23:48:37 -0300

    Agregamos controller reporte provincia
    Co-authored-by: juliadp <28408244+juliadp@users.noreply.github.com>

--- src/main/java/com/disenio/mimagrupo06/presentacion/ReportesController.java
+++ src/main/java/com/disenio/mimagrupo06/presentacion/ReportesController.java
@@ -1,17 +1,26 @@
 package com.disenio.mimagrupo06.presentacion;
 
+import com.disenio.mimagrupo06.presentacion.dto.ReporteProvincia;
+import com.disenio.mimagrupo06.repositorios.RepoReportes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class ReportesController {
 
-  /*@Autowired
+  @Autowired
   private RepoReportes repoReportes;
 
   @GetMapping("/hc_provincia")
   public ResponseEntity<List<ReporteProvincia>> obtenerHCPorProvincia(){
     List<ReporteProvincia> result = repoReportes.findAllHCPorSectorTerritorial();
     return ResponseEntity.status(200).body(result);
-  }*/
+  }
 
 }
