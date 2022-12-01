6d3cbad0f2f40570335db93809ff2e4449fdfd86
mercedesmagnelli <mechamagnelli@gmail.com>
2022-12-01 11:16:41 -0300

    más redirecciones
    Co-authored-by: mercedesmagnelli <62451237+mercedesmagnelli@users.noreply.github.com>

--- src/main/java/com/disenio/mimagrupo06/presentacion/TrayectosController.java
+++ src/main/java/com/disenio/mimagrupo06/presentacion/TrayectosController.java
@@ -27,7 +27,7 @@ public class TrayectosController {
     }
 
     @GetMapping("/registrarTrayecto")
-    public ResponseEntity<String> login() throws IOException {
+    public ResponseEntity<String> registrarTrayecto() throws IOException {
         //validar accion en capa modelo según roles o usuario asociados al idSesion
         Template template = handlebars.compile("/Template/eleccionCreacionTrayecto");
 
@@ -37,5 +37,43 @@ public class TrayectosController {
         String html = template.apply(model);
 
         return ResponseEntity.status(200).body(html);
+    }
+
+    @GetMapping("/registrarTrayectoNuevo")
+    public ResponseEntity<String> registrarTrayectoNuevo() throws IOException {
+        //validar accion en capa modelo según roles o usuario asociados al idSesion
+        Template template = handlebars.compile("/Template/registrarTrayectoNuevo");
+
+        Map<String, Object> model = new HashMap<>();
+        //model.put("listamascotas", mascotas);
+
+        String html = template.apply(model);
+
+        return ResponseEntity.status(200).body(html);
+    }
+
+    @GetMapping("/registrarTrayectoExistente")
+    public ResponseEntity<String> registrarTrayectoExistente() throws IOException {
+        //validar accion en capa modelo según roles o usuario asociados al idSesion
+        Template template = handlebars.compile("/Template/registrarTrayectoExistente");
+
+        Map<String, Object> model = new HashMap<>();
+        //model.put("listamascotas", mascotas);
+
+        String html = template.apply(model);
+
+        return ResponseEntity.status(200).body(html);
+    }
+
+    @PostMapping("/registrarTrayectoExistente")
+    public void recibirTrayectoExistente() throws IOException {
+        System.out.println("Estoy recibiendo un trayecto existente");
+    }
+
+    @PostMapping("/registrarTrayectoNuevo")
+    public void recibirTrayectoNuevo() throws IOException {
+
+        System.out.println("estoy recibiendo un trayecto nuevo");
+
     }
 }
--- src/main/resources/static/js/redireccionEntrePaginas.js
+++ src/main/resources/static/js/redireccionEntrePaginas.js
@@ -1,4 +1,6 @@
 document.getElementById("navRegistrarTrayecto").addEventListener('click', e=> {
     e.preventDefault()
-    cambiarUrlSinIdSesion("/registrarTrayecto")
+    cambiarUrlSinIdSesion("registrarTrayecto")
 })
+
+
